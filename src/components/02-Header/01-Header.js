baa4145ac5dace4b924028b6d4e949b1adc0cf57
Oguzhan Ulutas <mkoulutas@gmail.com>
2023-07-17 21:01:13 +0300

    Add main page layout

---
+++ src/components/02-Header/01-Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h1>Memory Game</h1>
+      <div className="score">
+        <div>Score :</div>
+        <div>Best Score :</div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/03-GameBoard/01-GameBoard.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const GameBoard = () => {
+  return <div className="game-board">GameBoard</div>;
+};
+
+export default GameBoard;
