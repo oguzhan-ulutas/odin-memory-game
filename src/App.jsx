73b9f14ad9db2559194009b9ef8a7a3b90f7b46e
Oguzhan Ulutas <mkoulutas@gmail.com>
2023-08-11 17:30:49 +0300

    Add handling score

--- src/App.jsx
+++ src/App.jsx
@@ -34,12 +34,120 @@ function App() {
     getImgs();
   }, [cardNumber]);
 
-  const handleScore = () => {};
+  // Keeping score
+  const [scoreArr, setScoreArr] = useState([]);
+
+  const shuffle = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  };
+
+  const handleScore = (e) => {
+    const cardID = e.target.className;
+    if (scoreArr.includes(cardID)) {
+      alert("Game over. Please start again.");
+      setTimeout(location.reload(), 1000);
+    } else if (score <= 3) {
+      // Adding card to array.
+      setScoreArr([...scoreArr, cardID]);
+      // setting Score and shuffling images
+      setScore(score + 1);
+      // Setting up next level if sroce reachs 3.
+      if (score === 2) {
+        setScoreArr([]);
+        setLevel(2);
+        setCardNumber(5);
+      }
+      setImgData(shuffle(imgData));
+    } else if (score >= 3 && score < 8) {
+      setScoreArr([...scoreArr, cardID]);
+      setScore(score + 1);
+      setImgData(shuffle(imgData));
+      console.log(score);
+
+      if (score === 7) {
+        setScoreArr([]);
+        setLevel(3);
+        setCardNumber(7);
+      }
+    } else if (score >= 8 && score < 15) {
+      setScoreArr([...scoreArr, cardID]);
+      setScore(score + 1);
+      setImgData(shuffle(imgData));
+
+      if (score === 14) {
+        setScoreArr([]);
+        setLevel(4);
+        setCardNumber(9);
+      }
+    } else if (score >= 15 && score < 24) {
+      setScoreArr([...scoreArr, cardID]);
+      setScore(score + 1);
+      setImgData(shuffle(imgData));
+
+      if (score === 23) {
+        setScoreArr([]);
+        setLevel(5);
+        setCardNumber(11);
+      }
+    } else if (score >= 24 && score < 35) {
+      setScoreArr([...scoreArr, cardID]);
+      setScore(score + 1);
+      setImgData(shuffle(imgData));
+
+      if (score === 34) {
+        setScoreArr([]);
+        setLevel(6);
+        setCardNumber(13);
+      }
+    } else if (score >= 35 && score < 48) {
+      setScoreArr([...scoreArr, cardID]);
+      setScore(score + 1);
+      setImgData(shuffle(imgData));
+
+      if (score === 47) {
+        setScoreArr([]);
+        setLevel(7);
+        setCardNumber(15);
+      }
+    } else if (score >= 48 && score < 63) {
+      setScoreArr([...scoreArr, cardID]);
+      setScore(score + 1);
+      setImgData(shuffle(imgData));
+
+      if (score === 62) {
+        setScoreArr([]);
+        setLevel(8);
+        setCardNumber(17);
+      }
+    } else if (score >= 63 && score < 80) {
+      setScoreArr([...scoreArr, cardID]);
+      setScore(score + 1);
+      setImgData(shuffle(imgData));
+
+      if (score === 79) {
+        setScoreArr([]);
+        setLevel(9);
+        setCardNumber(19);
+      }
+    } else if (score >= 80 && score < 99) {
+      setScoreArr([...scoreArr, cardID]);
+      setScore(score + 1);
+      setImgData(shuffle(imgData));
+
+      if (score === 98) {
+        alert("Congrats You finnished the game");
+      }
+    }
+  };
 
   return (
     <div className="app">
       <Header score={score} bestScore={bestScore} level={level} />
-      <GameBoard imgData={imgData} />
+      <GameBoard imgData={imgData} handleScore={handleScore} />
       <Footer />
     </div>
   );
--- src/components/03-GameBoard/01-GameBoard.jsx
+++ src/components/03-GameBoard/01-GameBoard.jsx
@@ -1,13 +1,17 @@
 import React, { useEffect } from "react";
 
 const GameBoard = (props) => {
-  const { imgData } = props;
+  const { imgData, handleScore } = props;
 
   const renderedImgData = imgData.map((item) => {
     return (
-      <div key={item.id} className={item.id}>
-        <img src={item.images.original.url} alt={item.title} />
-        <p>{item.title}</p>
+      <div key={item.id} className={item.id} onClick={handleScore}>
+        <img
+          className={item.id}
+          src={item.images.original.url}
+          alt={item.title}
+        />
+        <p className={item.id}>{item.title}</p>
       </div>
     );
   });
